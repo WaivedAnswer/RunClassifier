71f28f722d3e29fab9ff32c0221599d8819370bd
Quinn Ramsay <quinnramsay@gmail.com>
2019-10-04 12:25:44 -0600

    model weightings, and image. Still need to configure env
    Following tutorial: "https://medium.com/@guymodscientist/image-prediction-with-10-lines-of-code-3266f4039c7a"

---
+++ RunClassifier.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  4 10:04:31 2019
+
+@author: quinn.ramsay
+"""
+
+print("Hello")
+#from imageai.Prediction import ImagePrediction
+"""import os
+execution_path = os.getcwd()
+prediction = ImagePrediction()
+prediction.setModelTypeAsResNet()
+prediction.setModelPath( execution_path + "\resnet50_weights_tf_dim_ordering_tf_kernels.h5")
+prediction.loadModel()
+
+
+predictions, percentage_probabilities = prediction.predictImage(execution_path + "\runner.jpg", result_count=5)
+for index in range(len(predictions)):
+    print(predictions[index] , " : " , percentage_probabilities[index])"""
