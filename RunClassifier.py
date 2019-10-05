c9f03762efc29a4a16bc6f046d52b6443647a914
Quinn Ramsay <quinnramsay@gmail.com>
2019-10-05 07:53:01 -0600

    Working tutorial version
     Link was missing pillow library from steps

--- RunClassifier.py
+++ RunClassifier.py
@@ -5,16 +5,15 @@ Created on Fri Oct  4 10:04:31 2019
 @author: quinn.ramsay
 """
 
-print("Hello")
-#from imageai.Prediction import ImagePrediction
-"""import os
-execution_path = os.getcwd()
+from imageai.Prediction import ImagePrediction
+
 prediction = ImagePrediction()
 prediction.setModelTypeAsResNet()
-prediction.setModelPath( execution_path + "\resnet50_weights_tf_dim_ordering_tf_kernels.h5")
+prediction.setModelPath("resnet50_weights_tf_dim_ordering_tf_kernels.h5")
+print (prediction.modelPath)
 prediction.loadModel()
 
 
-predictions, percentage_probabilities = prediction.predictImage(execution_path + "\runner.jpg", result_count=5)
+predictions, percentage_probabilities = prediction.predictImage("runner.jpg", result_count=5)
 for index in range(len(predictions)):
-    print(predictions[index] , " : " , percentage_probabilities[index])"""
+    print(predictions[index] , " : " , percentage_probabilities[index])
